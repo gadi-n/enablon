feaca275c34136d1c3223408f556d5d6876287e2
gadinehabpagaya <gadi.nehab@pagaya.com>
2024-04-27 14:36:20 +0300

    homepage structure test

---
+++ test_positive.py
@@ -0,0 +1,32 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def open_url(browser):
+    url = "https://todomvc.com/examples/react/dist/"
+    browser.get(url)
+
+
+def test_homepage_structure(browser, open_url):
+    # verify header
+    header_text = browser.find_element(By.CSS_SELECTOR, "h1").text
+    assert header_text == "todos", f'Error! Header text is {header_text} and not "todos"'
+    # verify placeholder
+    placeholder_text = browser.find_element(By.ID, "todo-input").get_attribute("placeholder")
+    assert placeholder_text == 'What needs to be done?', f'Error! Place holder text is {placeholder_text} and not "What needs to be done"?'
+    # verify footer
+    footer_text = browser.find_element(By.CLASS_NAME, "info").text
+    assert footer_text == "Double-click to edit a todo\nCreated by the TodoMVC Team\nPart of TodoMVC", \
+        f'Error! footer text text is {footer_text} and not "Double-click to edit a todo\nCreated by the TodoMVC Team\nPart of TodoMVC"'
+    # verify link
+    href_value = browser.find_element(By.LINK_TEXT, "TodoMVC").get_attribute("href")
+    assert href_value == "http://todomvc.com/", f'link leads to {href_value} and not to "http://todomvc.com/"'
