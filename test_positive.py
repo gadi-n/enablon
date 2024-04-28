9a7d527b2f89d75bfc123c1aed598b5fb072ff3a
gadinehabpagaya <gadi.nehab@pagaya.com>
2024-04-28 12:26:31 +0300

    create clear completed tasks test

--- test_positive.py
+++ test_positive.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
+TASK_ELEMENT = "//*[@data-testid='todo-item-label']"
+CHECKBOX_ELEMENT = "//input[@class='toggle' and @type='checkbox' and @data-testid='todo-item-toggle']"
+
 @pytest.fixture
 def browser():
     driver = webdriver.Chrome()
@@ -76,23 +79,35 @@ def test_create_several_tasks(browser, open_url, textbox_element):
 def test_complete_task_and_display_by_status(browser, open_url, textbox_element):
     # preconditions - create tasks
     create_tasks(browser, 5, 'task', textbox_element)
-    checkboxes = (browser.find_elements
-                  (By.XPATH, "//input[@class='toggle' and @type='checkbox' and @data-testid='todo-item-toggle']"))
+    checkboxes = (browser.find_elements (By.XPATH, CHECKBOX_ELEMENT))
     # complete tasks
     checkboxes[0].click()
     checkboxes[2].click()
     # show only completed
     browser.find_element(By.LINK_TEXT, "Completed").click()
-    completed_tasks = browser.find_elements(By.XPATH, "//*[@data-testid='todo-item-label']")
+    completed_tasks = browser.find_elements(By.XPATH, TASK_ELEMENT)
     assert len(completed_tasks) == 2, f'Error! there are {len(completed_tasks)} completed tasks but there should be 2'
     validate_task_appears('task', [1, 3], completed_tasks)
     # show only active
     browser.find_element(By.LINK_TEXT, "Active").click()
-    active_tasks = browser.find_elements(By.XPATH, "//*[@data-testid='todo-item-label']")
+    active_tasks = browser.find_elements(By.XPATH, TASK_ELEMENT)
     assert len(active_tasks) == 3, f'Error! there are {len(active_tasks)} active tasks but there should be 3'
     validate_task_appears('task', [2, 4, 5], active_tasks)
     # show all tasks
     browser.find_element(By.LINK_TEXT, "All").click()
-    all_tasks = browser.find_elements(By.XPATH, "//*[@data-testid='todo-item-label']")
+    all_tasks = browser.find_elements(By.XPATH, TASK_ELEMENT)
     assert len(all_tasks) == 5, f'Error! there are {len(all_tasks)} tasks but there should be 5'
     validate_task_appears('task', [1, 2, 3, 4, 5], all_tasks)
+
+
+def test_clear_completed_tasks(browser, open_url, textbox_element):
+    # preconditions - create and complete tasks
+    create_tasks(browser, 3, 'task', textbox_element)
+    checkboxes = (browser.find_elements(By.XPATH, CHECKBOX_ELEMENT))
+    for checkbox in checkboxes:
+        checkbox.click()
+    # clear completed tasks
+    browser.find_element(By.CLASS_NAME, "clear-completed").click()
+    # validate tasks cleared
+    all_tasks = browser.find_elements(By.XPATH, TASK_ELEMENT)
+    assert len(all_tasks) == 0, f'Error! there are {len(all_tasks)} tasks but there should be 0'
