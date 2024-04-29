5d882ca5a95f62ff9629e30a726e7be369b78831
gadinehabpagaya <gadi.nehab@pagaya.com>
2024-04-29 15:45:14 +0300

    rearrange files

--- conftest.py
+++ conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 
 
